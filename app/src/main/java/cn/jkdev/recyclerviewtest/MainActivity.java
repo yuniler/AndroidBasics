dab325c511ac39de60a8f4f87d379c545f60fbdd
sunguangyuniler@163.com <sunguangyuniler@163.com>
2021-03-05 21:09:24 +0800

    recyclerViewDemo

---
+++ app/src/main/java/cn/jkdev/recyclerviewtest/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.jkdev.recyclerviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //dada
+        setContentView(R.layout.activity_main);
+    }
+}
